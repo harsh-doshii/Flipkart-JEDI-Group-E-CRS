28e59ca8492af367a70af6c72de8abf65544d01f
Harsh Doshi <doshiharsh26@gmail.com>
2023-01-30 16:37:04 +0530

    User

---
+++ CRS-JEDI-JAVA-Development-POS/src/com/flipkart/bean/User.java
@@ -0,0 +1,78 @@
+package com.flipkart.bean;
+
+public abstract class User {
+    private String name;
+    private String gender;
+    private String address;
+    private String username;
+    private String password;
+    private int id;
+    private String role;
+    private String dob;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+
+
+}
