9b30f27ce8056c17a015a2fd4e8a4d019d9131a1
manav-bhagchandani <manav.bhagchandani@flipkart.com>
2023-01-30 16:50:46 +0530

    Admin and Student added

---
+++ CRS-JEDI-JAVA-Development-POS/src/com/flipkart/bean/Admin.java
@@ -0,0 +1,15 @@
+package com.flipkart.bean;
+
+public class Admin extends User {
+    private String adminId;
+
+    public String getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(String adminId) {
+        this.adminId = adminId;
+    }
+
+
+}
---
+++ CRS-JEDI-JAVA-Development-POS/src/com/flipkart/bean/Student.java
@@ -0,0 +1,42 @@
+package com.flipkart.bean;
+
+import java.util.List;
+
+public class Student extends User{
+    private Branch branch;
+    private int semester;
+    private List coursesEnrolled;
+    private List preferenceList;
+
+    public Branch getBranch() {
+        return branch;
+    }
+
+    public void setBranch(Branch branch) {
+        this.branch = branch;
+    }
+
+    public int getSemester() {
+        return semester;
+    }
+
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
+
+    public List getCoursesEnrolled() {
+        return coursesEnrolled;
+    }
+
+    public void setCoursesEnrolled(List coursesEnrolled) {
+        this.coursesEnrolled = coursesEnrolled;
+    }
+
+    public List getPreferenceList() {
+        return preferenceList;
+    }
+
+    public void setPreferenceList(List preferenceList) {
+        this.preferenceList = preferenceList;
+    }
+}
