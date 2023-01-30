e6671c494e945465d5772ba46626f25cdb56aeec
*Bhavya280* <*bhavya.b@flipkart.com*>
2023-01-30 22:20:24 +0530

    AdminCRS added

---
+++ CRS-JEDI-JAVA-Development-POS/src/com/flipkart/client/AdminCRS.java
@@ -0,0 +1,29 @@
+package com.flipkart.client;
+
+public class AdminCRS {
+    public void printAdminMenu() {
+    }
+
+    private void generateReportCard() {
+
+    }
+    private void addProfessor() {
+
+    }
+    private void validateStudentRegistration() {
+
+    }
+    private void removeProfessor() {
+
+    }
+
+    private void removeCourse() {
+
+    }
+    private void addCourse() {
+
+    }
+    private void logout() {
+
+    }
+}
