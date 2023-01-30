6ca1e7a7d219416fde35078d35a93dc006996ebd
Harsh Doshi <doshiharsh26@gmail.com>
2023-01-30 16:52:47 +0530

    course,regcourse

---
+++ CRS-JEDI-JAVA-Development-POS/src/com/flipkart/bean/RegisteredCourse.java
@@ -0,0 +1,34 @@
+package com.flipkart.bean;
+
+public class RegisteredCourse {
+    private Course course;
+
+    private Student student;
+    private int semester;
+
+    //    private Grade grade;
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public int getSemester() {
+        return semester;
+    }
+
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
+
+}
