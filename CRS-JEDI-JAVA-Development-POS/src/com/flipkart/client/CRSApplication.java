2afc03f4ee711a0d2a69ccc08b4ff3ea3aedaf53
whoAmI-3 <aman.jham@flipkart.com>
2023-01-31 09:36:28 +0530

    added CRSApplication Client

---
+++ CRS-JEDI-JAVA-Development-POS/src/com/flipkart/client/CRSApplication.java
@@ -0,0 +1,27 @@
+package com.flipkart.client;
+
+public class CRSApplication {
+    public static void main(String[] args) {
+        MainMenu();
+    }
+
+    public static void mainMenu() {
+
+    }
+
+    private static void exit() {
+        System.out.println("Exit App");
+    }
+
+    private static void login() {
+
+    }
+
+    private static void register() {
+
+    }
+
+    private static void loginMain(String userId, String password) {
+
+    }
+}
