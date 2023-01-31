f8f5cbcc3088462088aa0d086af95c7aedb63c60
Harsh Doshi <doshiharsh26@gmail.com>
2023-01-31 10:32:53 +0530

    added grade class

---
+++ CRS-JEDI-JAVA-Development-POS/src/com/flipkart/bean/Grade.java
@@ -0,0 +1,13 @@
+package com.flipkart.bean;
+
+public class Grade {
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    private String grade;
+}
