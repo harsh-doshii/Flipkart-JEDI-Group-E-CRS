542b28f9f24a6fd5e680714d144e887e00d267af
*Bhavya280* <*bhavya.b@flipkart.com*>
2023-01-31 15:28:01 +0530

    added professor menu

--- CRS-JEDI-JAVA-Development-POS/src/com/flipkart/client/ProfessorCRSMenu.java
+++ CRS-JEDI-JAVA-Development-POS/src/com/flipkart/client/ProfessorCRSMenu.java
@@ -1,5 +1,5 @@
 package com.flipkart.client;
-
+import java.util.Scanner;
 public class ProfessorCRSMenu {
 
     private String professorID;
@@ -7,8 +7,44 @@ public class ProfessorCRSMenu {
 
 
     public ProfessorCRSMenu(String profId){
+
         this.professorID = profId;
     }
+
+    public void displayMenu() {
+        // Display the options available for the Professor
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Welcome to Professor Menu! Enter option :");
+            System.out.println("1. View Courses");
+            System.out.println("2. View Registered Students");
+            System.out.println("3. Assign grade");
+            System.out.println("4. Logout\n");
+            System.out.println("Enter Option : ");
+
+            int input = sc.nextInt();
+            sc.nextLine();
+
+            if(input==1)
+                viewCourse();
+            else if(input==2)
+                viewStudents();
+
+            else if(input==3)
+                assignGrades();
+
+            else if(input==4) {
+                logout();
+            }
+            else
+                System.out.println("Invalid choice");
+        }
+    }
+
+
+
+
     private  void viewCourse(){
         System.out.printf("view course");
     }
@@ -19,5 +55,9 @@ public class ProfessorCRSMenu {
 
     private  void assignGrades(){
         System.out.printf("assign grades");
+    }
+
+    private void logout() {
+        System.out.println("Loging out");
     }
 }
