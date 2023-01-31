6d977a9cb905834c4a5c0cc5c063e96f9a169092
manav-bhagchandani <manav.bhagchandani@flipkart.com>
2023-01-31 09:54:55 +0530

    added branch class details and permission updated

--- CRS-JEDI-JAVA-Development-POS/src/com/flipkart/bean/Branch.java
+++ CRS-JEDI-JAVA-Development-POS/src/com/flipkart/bean/Branch.java
@@ -1,4 +1,31 @@
 package com.flipkart.bean;
 
 public class Branch {
+    private String id;
+    private String name;
+    private Professor hod;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Professor getHod() {
+        return hod;
+    }
+
+    public void setHod(Professor hod) {
+        this.hod = hod;
+    }
 }
