4adcdce6b18d353be0e77bf7b362174d619e105c
whoAmI-3 <aman.jham@flipkart.com>
2023-02-02 14:26:27 +0530

    implemented assign grade function

--- CRS-JEDI-JAVA-Development-POS/src/com/flipkart/service/ProfessorServiceOperation.java
+++ CRS-JEDI-JAVA-Development-POS/src/com/flipkart/service/ProfessorServiceOperation.java
@@ -2,34 +2,46 @@ package com.flipkart.service;
 
 import com.flipkart.bean.Course;
 import com.flipkart.bean.Grade;
+import com.flipkart.bean.RegisteredCourse;
 import com.flipkart.bean.Student;
 import com.flipkart.data.TempData;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Set;
 
 public class ProfessorServiceOperation implements ProfessorService{
 
     @Override
     public List<Course> viewCourses(int profId) {
+
         List<Course> courses = TempData.profToCourses.get(profId);
         return courses;
     }
 
     @Override
     public List<Student> viewStudents(int courseId) {
+
+        /*
+        * Have to think if we need profID
+        *
+        */
         List<Student> enrolledStudents = TempData.courseToEnrolledStudents.get(courseId);
         return enrolledStudents;
     }
 
     @Override
-    public void assignGrade(int studentId, int courseId, Grade grade) {
+    public boolean assignGrade(int studentId, int courseId, Grade grade) {
         /*
         * Will see after student service.
         **/
-//        TempData.profGivingGrade.put(studentId, );
-        return;
+        for(var course : TempData.studentToRegisteredCourseList.get(studentId)) {
+            if(course.getCourse().getCourseId()==courseId) {
+                course.setGrade(grade);
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
